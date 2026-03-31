7357d6c0eec8f90a05fb2f20ab7bfffe8e578056
MRTarmak <tarasenkovmaksim39@gmail.com>
2026-03-31 17:51:55 +0300

    Update profiles repository, add profiles device

--- app/iot_gardener/lib/data/repositories_impl/monitoring_profiles_repository_impl.dart
+++ app/iot_gardener/lib/data/repositories_impl/monitoring_profiles_repository_impl.dart
@@ -7,28 +7,48 @@ import '../../domain/repositories/monitoring_profiles_repository.dart';
 
 class MonitoringProfilesRepositoryImpl extends MonitoringProfilesRepository {
   static const String _profilesKey = 'monitoring_profiles';
+  static const String _profilesNamesKey = 'monitoring_profiles_names';
 
   @override
   Future<List<MonitoringProfileParams>> getMonitoringProfiles() async {
     final prefs = await SharedPreferences.getInstance();
-    final profiles = prefs.getStringList(_profilesKey) ?? [];
+    final names = prefs.getStringList(_profilesNamesKey) ?? [];
 
-    return profiles
-        .map(
-          (raw) => MonitoringProfileParams.fromMap(
-            jsonDecode(raw) as Map<String, dynamic>,
-          ),
-        )
-        .toList();
+    List<MonitoringProfileParams> profiles = [];
+    for (var name in names) {
+      final jsonStr = prefs.getString('$_profilesKey$name');
+      if (jsonStr != null) {
+        final json = jsonDecode(jsonStr);
+        profiles.add(MonitoringProfileParams.fromMap(json as Map<String, dynamic>));
+      }
+    }
+    return profiles;
   }
 
   @override
   Future<void> addMonitoringProfile(MonitoringProfileParams params) async {
-    // TODO
+    final prefs = await SharedPreferences.getInstance();
+    final names = prefs.getStringList(_profilesNamesKey) ?? [];
+    if (!names.contains(params.name)) {
+      final jsonStr = jsonEncode(params);
+      await prefs.setString('$_profilesKey${params.name}', jsonStr);
+      names.add(params.name);
+      await prefs.setStringList(_profilesNamesKey, names);
+      return;
+    }
+    throw Exception('Profile with name ${params.name} already exists');
   }
 
   @override
   Future<void> deleteMonitoringProfileByName(String name) async {
-    // TODO
+    final prefs = await SharedPreferences.getInstance();
+    final names = prefs.getStringList(_profilesNamesKey) ?? [];
+    if (names.contains(name)) {
+      await prefs.remove('$_profilesKey$name');
+      names.remove(name);
+      await prefs.setStringList(_profilesNamesKey, names);
+      return;
+    }
+    throw Exception('Profile with name $name does not exist');
   }
 }
