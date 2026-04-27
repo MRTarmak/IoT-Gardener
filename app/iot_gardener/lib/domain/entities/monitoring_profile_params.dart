ce250fcfb571dd377a388f464c4dc9ca65f2f0d2
MRTarmak <tarasenkovmaksim39@gmail.com>
2026-04-27 23:23:05 +0300

    Reorder soil and environmental parameters everywhere for consistency

--- app/iot_gardener/lib/domain/entities/monitoring_profile_params.dart
+++ app/iot_gardener/lib/domain/entities/monitoring_profile_params.dart
@@ -2,31 +2,31 @@ import '../utils/converter.dart';
 
 class MonitoringProfileParams {
   final String name;
-  final (double?, double?) soilMoistureRange;
-  final (double?, double?) airHumidityRange;
-  final (double?, double?) soilPhRange;
   final (double?, double?) soilTemperatureRange;
+  final (double?, double?) soilMoistureRange;
+  final (double?, double?) soilPhRange;
   final (double?, double?) airTemperatureRange;
+  final (double?, double?) airHumidityRange;
   final (double?, double?) lightRange;
 
   const MonitoringProfileParams({
     required this.name,
-    this.soilMoistureRange = (null, null),
-    this.airHumidityRange = (null, null),
-    this.soilPhRange = (null, null),
     this.soilTemperatureRange = (null, null),
+    this.soilMoistureRange = (null, null),
+    this.soilPhRange = (null, null),
     this.airTemperatureRange = (null, null),
+    this.airHumidityRange = (null, null),
     this.lightRange = (null, null),
   });
 
   factory MonitoringProfileParams.fromMap(Map<String, dynamic> map) {
     return MonitoringProfileParams(
       name: map['name'] ?? 'Unnamed Profile',
-      soilMoistureRange: Converter.toRange(map['soilMoistureRange']),
-      airHumidityRange: Converter.toRange(map['airHumidityRange']),
-      soilPhRange: Converter.toRange(map['soilPhRange']),
       soilTemperatureRange: Converter.toRange(map['soilTemperatureRange']),
+      soilMoistureRange: Converter.toRange(map['soilMoistureRange']),
+      soilPhRange: Converter.toRange(map['soilPhRange']),
       airTemperatureRange: Converter.toRange(map['airTemperatureRange']),
+      airHumidityRange: Converter.toRange(map['airHumidityRange']),
       lightRange: Converter.toRange(map['lightRange']),
     );
   }
--- app/iot_gardener/lib/domain/entities/telemetry_data.dart
+++ app/iot_gardener/lib/domain/entities/telemetry_data.dart
@@ -1,31 +1,31 @@
 import '../utils/converter.dart';
 
 class TelemetryData {
-  final double soilMoisture;
-  final double airHumidity;
-  final double soilPh;
   final double soilTemperature;
+  final double soilMoisture;
+  final double soilPh;
   final double airTemperature;
+  final double airHumidity;
   final double light;
   final DateTime receivedAt;
 
   const TelemetryData({
-    required this.soilMoisture,
-    required this.airHumidity,
-    required this.soilPh,
     required this.soilTemperature,
+    required this.soilMoisture,
+    required this.soilPh,
     required this.airTemperature,
+    required this.airHumidity,
     required this.light,
     required this.receivedAt,
   });
 
   factory TelemetryData.fromMap(Map<String, dynamic> map) {
     return TelemetryData(
-      soilMoisture: Converter.toDouble(map['soilMoisture']),
-      airHumidity: Converter.toDouble(map['airHumidity']),
-      soilPh: Converter.toDouble(map['soilPh']),
       soilTemperature: Converter.toDouble(map['soilTemperature']),
+      soilMoisture: Converter.toDouble(map['soilMoisture']),
+      soilPh: Converter.toDouble(map['soilPh']),
       airTemperature: Converter.toDouble(map['airTemperature']),
+      airHumidity: Converter.toDouble(map['airHumidity']),
       light: Converter.toDouble(map['light']),
       receivedAt: DateTime.now(),
     );
